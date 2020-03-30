77c7e81fcbab6a4a55f02d0fdf022cb5ca574972
BaoTong121 <240741220@qq.com>
2020-03-30 11:03:51 +0800

    09

--- perms/models.py
+++ perms/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class Perm(models.Model):
     name = models.CharField(max_length=64, verbose_name="名称")
     user = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name='用户')
-    asset = models.ManyToMannuyField(Assets, verbose_name='资产')
+    asset = models.ManyToManyField(Assets, verbose_name='资产')
     comment = models.CharField(max_length=128, blank=True, null=True)
     date_add = models.DateTimeField(auto_now_add=True)
 
