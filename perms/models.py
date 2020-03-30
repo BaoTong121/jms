593bf147c8bd5e00e1163fc4d34626be5f739410
BaoTong121 <240741220@qq.com>
2020-03-30 11:02:20 +0800

    08

--- perms/models.py
+++ perms/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+
 from assets.models import Assets
 from django.conf import settings
 
 
-class Perms(models.Model):
-    name = models.CharField(max_length=60)
-    user = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    asset = models.ManyToManyField(Assets)
+class Perm(models.Model):
+    name = models.CharField(max_length=64, verbose_name="名称")
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name='用户')
+    asset = models.ManyToMannuyField(Assets, verbose_name='资产')
     comment = models.CharField(max_length=128, blank=True, null=True)
     date_add = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+
 
 # Create your models here.
