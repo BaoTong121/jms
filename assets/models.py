894d91860acc7b4c73b0a66be0831b7b84f2a5ce
BaoTong121 <240741220@qq.com>
2020-03-30 10:54:01 +0800

    07

--- assets/models.py
+++ assets/models.py
@@ -12,7 +12,7 @@ class Assets(models.Model):
     ip = models.GenericIPAddressField(verbose_name='IP', unique=True)
     port = models.IntegerField(default=22, verbose_name='端口')
     username = models.CharField(max_length=20, verbose_name='管理员用户名')
-    password = models.CharField(max_length=2, verbose_name='管理员密码')
+    password = models.CharField(max_length=20, verbose_name='管理员密码')
     os = models.CharField(choices=OS_CHOICE, max_length=2, verbose_name='系统平台', default="C")
     is_active = models.BooleanField(default=True, verbose_name="是否激活")
 
